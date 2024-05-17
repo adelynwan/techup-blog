e66521c6946bcd90fe3c334e2595bc332245a58c
adelynwan <adelynsmartnation@gmail.com>
2024-05-17 03:03:31 +0000

    amended

--- server.js
+++ server.js
@@ -98,3 +98,8 @@ app.post("/delete/:id", async (req, res) => {
 
 // Tells the app which port to run on
 app.listen(8080);
+
+// demo function
+app.get('/demo', function(req, res) {
+  res.render('pages/demo');
+});
